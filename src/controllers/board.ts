33458c71fc7df8505e508d2c3035e19f1e60c6c0
Daniel Stephany <danielstephany85@gmail.com>
2024-07-10 22:42:46 -0700

    updated board endpoint

--- src/controllers/board.ts
+++ src/controllers/board.ts
@@ -3,31 +3,47 @@ import Board from "../models/board"
 import { Request, Response, NextFunction } from 'express'
 import iError from '../types/error'
 
+interface iBoardData  {
+    title: string,
+    columns: string[]
+    usersWithAccess: string[]
+}
+
+type tColumnMap = {[key: string]: {
+    title: string,
+    columnId: string,
+}}
 
 export const createBoard = async (req: Request, res: Response, next: NextFunction) => {
-
+    const columnMap: tColumnMap = {}
+    const columnOrder: String[] = []
     try {
-        const board = await Board.create({
-            owner: res.locals.userId,
-            title: "Test Board",
-            columns: {
-                "ready": {
-                    columnId: "ready",
-                    title: "Ready"
-                },
-                "in-progress": {
-                    columnId: "column2",
-                    title: "In Progress"
-                },
-                "done": {
-                    columnId: "done",
-                    title: "Done"
-                },
-            },
-            columnOrder: ["Ready", "In Progress", "Done"],
-            usersWithAccess: []
-        })
-        res.status(200).json(board)
+        const { title, columns, usersWithAccess}: iBoardData = req.body
+
+        if(title && Array.isArray(columns)){
+            columns.forEach(columnTitle => {
+                const titleKey = columnTitle.toLowerCase().split(" ").join("-")
+                columnMap[titleKey] = {
+                    title: columnTitle,
+                    columnId: titleKey
+                }
+                columnOrder.push(titleKey)
+            })
+
+            const board = await Board.create({
+                owner: res.locals.userId,
+                title,
+                columns: columnMap,
+                columnOrder,
+                usersWithAccess,
+            })
+            res.status(200).json(board)
+        } else {
+            const error: iError = new Error("title, columns, and usersWithAccess are required")
+            error.statusCode = 402
+            throw error
+        }
+
 
     } catch (e) {
         const err = e as iError
--- src/models/board.ts
+++ src/models/board.ts
@@ -10,6 +10,10 @@ const columnSchema = new mongoose.Schema({
 });
 
 const boardSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
